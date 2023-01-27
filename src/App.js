60c5430a9e6c3d5f60cd16c18b4aca05b5c2cec4
vamsikrishnamangu <108578253+vamsikrishnamangu@users.noreply.github.com>
2023-01-27 18:17:14 +0530

    feat: added switch to route in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 
@@ -6,8 +6,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Login />}/>
-        <Route exact path="/dashboard" element={<Dashboard />}/>
+        <Switch>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Switch>
       </Routes>
     </BrowserRouter>
   );
