83208344f551945575a5a5f68252998847d7c17a
vamsikrishnamangu <108578253+vamsikrishnamangu@users.noreply.github.com>
2023-01-27 19:53:04 +0530

    feat: added contact and logout buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,39 +1,81 @@
 import React, { Component } from "react";
 import logo from "../assets/palnesto_logo.png";
-import invoice from '../assets/invoice.pdf';
+import invoice from "../assets/invoice.pdf";
+import { Link } from "react-router-dom";
+
 class Dashboard extends Component {
   state = {
     company: "SpringBoard",
-    paymentDate: "25/1/2022",
-    expiryDate: "25/1/2023",
+    paymentDate: "25/1/2023",
+    expiryDate: "25/1/2024",
   };
-
   render() {
     const { company, paymentDate, expiryDate } = this.state;
     return (
       <>
-        <div>
-          <img
-            src={logo}
-            alt="palnesto-logo"
-            style={{height:"5%",width:"5%"}}
-          />
-        </div>
         <div
           style={{
             display: "flex",
             flexDirection: "row",
-            justifyContent: "center",
-            alignSelf: "center",
+            justifyContent: "space-between",
           }}
         >
+          <div style={{ height: "15%", width: "15%" }}>
+            <img src={logo} alt="palnesto-logo" />
+          </div>
+          <div>
+            <button
+              style={{
+                color: "#ffffff",
+                textDecoration: "none",
+                height: "50%",
+                marginRight: "50px",
+                marginTop: "20px",
+                backgroundColor: "#FA5D04",
+                borderRadius: "10px",
+                borderStyle: "none",
+              }}
+            >
+              Contact Manager - 8886911466
+            </button>
+            <Link to="/">
+              <button
+                style={{
+                  color: "#ffffff",
+                  textDecoration: "none",
+                  height: "50%",
+                  marginRight: "50px",
+                  marginTop: "20px",
+                  backgroundColor: "#FA5D04",
+                  borderRadius: "10px",
+                  borderStyle: "none",
+                }}
+              >
+                Logout
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignSelf: "center",
+            marginLeft: "10%",
+          }}
+        >
+          <div>
+            <h1 style={{ marginLeft: "1%", fontSize: "30px" }}>
+              Palnesto Hosting Solutions
+            </h1>
+          </div>
           <div
             style={{
               color: "#595959",
               boxShadow: "0px 4px 16px rgba(105, 105, 105, 0.25)",
-              height:"60vh",
-              width:"75vw"
-
+              height: "60vh",
+              width: "75vw",
             }}
           >
             <form onSubmit={this.submitForm}>
@@ -45,9 +87,9 @@ class Dashboard extends Component {
                   borderColor: "#BFBFBF",
                   borderRadius: "5px",
                   borderStyle: "solid",
-                  margin:"1%",
-                  paddingLeft:"2%",
-                  paddingRight:"2%"
+                  margin: "1%",
+                  paddingLeft: "2%",
+                  paddingRight: "2%",
                 }}
               >
                 <p style={{ fontWeight: 700 }}>Name of the company</p>
@@ -61,9 +103,9 @@ class Dashboard extends Component {
                   borderColor: "#BFBFBF",
                   borderRadius: "5px",
                   borderStyle: "solid",
-                  margin:"1%",
-                  paddingLeft:"2%",
-                  paddingRight:"2%"
+                  margin: "1%",
+                  paddingLeft: "2%",
+                  paddingRight: "2%",
                 }}
               >
                 <p style={{ fontWeight: 700 }}>Payment Date</p>
@@ -77,31 +119,35 @@ class Dashboard extends Component {
                   borderColor: "#BFBFBF",
                   borderRadius: "5px",
                   borderStyle: "solid",
-                  margin:"1%",
-                  paddingLeft:"2%",
-                  paddingRight:"2%"
+                  margin: "1%",
+                  paddingLeft: "2%",
+                  paddingRight: "2%",
                 }}
               >
                 <p style={{ fontWeight: 700 }}>Expiry Date</p>
                 <p>{expiryDate}</p>
               </div>
-              <div style={{textAlign:"center", marginTop:"10%"}}>
-                  <a href={invoice} download="invoice" target="_blank" rel="noreferrer"
-                    style={{
-                      borderRadius: "12px",
-                      backgroundColor: "#FEC400",
-                      color: "#ffffff",
-                      borderStyle: "none",
-                      paddingLeft:"5%",
-                      paddingRight:"5%",
-                      paddingTop:"1%",
-                      paddingBottom:"1%",
-                      fontSize:"1em",
-                      textDecoration:"none"
-                    }}
-                  >
-                    Download Invoice
-                  </a>
+              <div style={{ textAlign: "center", marginTop: "10%" }}>
+                <a
+                  href={invoice}
+                  download="invoice"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    borderRadius: "12px",
+                    backgroundColor: "#FEC400",
+                    color: "#ffffff",
+                    borderStyle: "none",
+                    paddingLeft: "5%",
+                    paddingRight: "5%",
+                    paddingTop: "1%",
+                    paddingBottom: "1%",
+                    fontSize: "1em",
+                    textDecoration: "none",
+                  }}
+                >
+                  Download Invoice
+                </a>
               </div>
             </form>
           </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,20 +2,25 @@ import React, { Component } from "react";
 import logo from "../assets/palnesto_logo.png";
 import { Navigate } from "react-router-dom";
 class Login extends Component {
-  state = { email: "", password: "",error:false,errorMsg:"Invalid email or password",isCredentialsValid:false };
+  state = {
+    email: "",
+    password: "",
+    error: false,
+    errorMsg: "Invalid email or password",
+    isCredentialsValid: false,
+  };
   submitForm = (event) => {
     event.preventDefault();
     const { email, password } = this.state;
-    if (email === "springboard@gmail.com" && password === "12345678") {
+    if (email === "springboardschool1@gmail.com" && password === "Palspring@456") {
       this.setState({
-        isCredentialsValid:true
+        isCredentialsValid: true,
       });
-      console.log("successful");
-    }else{
-        this.setState(prevState=>({
-          error:!prevState.error,
-        }));
-        this.setState({errorMsg:"Invalid email or password"});
+    } else {
+      this.setState((prevState) => ({
+        error: !prevState.error,
+      }));
+      this.setState({ errorMsg: "Invalid email or password" });
     }
   };
 
@@ -26,37 +31,67 @@ class Login extends Component {
     this.setState({ password: event.target.value });
   };
   render() {
-    const { email, password,errorMsg,error,isCredentialsValid} = this.state;
+    const { email, password, errorMsg, error, isCredentialsValid } = this.state;
     return (
-      <>{isCredentialsValid && <Navigate to = '/dashboard' replace={true} />}
-        <div>
-          <img
-            src={logo}
-            alt="palnesto-logo"
-            style={{height:"5%",width:"5%"}}
-          />
-        </div>
+      <>
+        {isCredentialsValid && <Navigate to="/dashboard" replace={true} />}
         <div
           style={{
             display: "flex",
             flexDirection: "row",
-            justifyContent: "center",
-            alignSelf: "center",
+            justifyContent: "space-between",
           }}
         >
+          <div style={{ height: "15%", width: "15%" }}>
+            <img src={logo} alt="palnesto-logo" />
+          </div>
+          <div>
+            <button
+              style={{
+                color: "#ffffff",
+                textDecoration: "none",
+                height: "50%",
+                marginRight: "50px",
+                marginTop: "20px",
+                backgroundColor: "#FA5D04",
+                borderRadius: "10px",
+                borderStyle: "none",
+              }}
+            >
+              Contact Manager - 8886911466
+            </button>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignSelf: "center",
+            marginLeft: "10%",
+          }}
+        >
+          <div>
+            <h1 style={{ marginLeft: "10%", fontSize: "30px" }}>
+              Palnesto Hosting Solutions
+            </h1>
+          </div>
           <div
             style={{
               color: "#595959",
               boxShadow: "0px 4px 16px rgba(105, 105, 105, 0.25)",
-              height:"50%",
-              width:"75%",
-              paddingBottom:"2%"
+              height: "50%",
+              width: "75%",
+              paddingBottom: "2%",
             }}
           >
             <form onSubmit={this.submitForm}>
-              <h6 style={{marginLeft:"10%",fontSize: "30px" }}>Login</h6>
+              <h6 style={{ marginLeft: "10%", fontSize: "30px" }}>Login</h6>
               <div>
-                <label htmlFor="email" style={{ fontSize: "30px",marginLeft:"10%"}}>
+                <label
+                  htmlFor="email"
+                  style={{ fontSize: "30px", marginLeft: "10%" }}
+                >
                   Email
                 </label>
                 <br />
@@ -67,21 +102,21 @@ class Login extends Component {
                   type="email"
                   placeholder="abc@gmail.com"
                   style={{
-                    marginLeft:"10%",
-                    marginBottom:"5%",
+                    marginLeft: "10%",
+                    marginBottom: "5%",
                     borderColor: "#929292",
                     borderWidth: "1px",
                     borderStyle: "solid",
                     borderRadius: "10px",
-                    width:"60%",
-                    padding:"1.5%"
+                    width: "60%",
+                    padding: "1.5%",
                   }}
                 />
               </div>
               <div>
                 <label
                   htmlFor="password"
-                  style={{marginLeft:"10%",fontSize:"30px"}}
+                  style={{ marginLeft: "10%", fontSize: "30px" }}
                 >
                   Password
                 </label>
@@ -92,36 +127,45 @@ class Login extends Component {
                   id="password"
                   type="password"
                   style={{
-                    marginLeft:"10%",
-                    marginBottom:"5%",
+                    marginLeft: "10%",
+                    marginBottom: "5%",
                     borderColor: "#929292",
                     borderWidth: "1px",
                     borderStyle: "solid",
                     borderRadius: "12px",
-                    width:"60%",
-                    padding:"1.5%"
+                    width: "60%",
+                    padding: "1.5%",
                   }}
                 />
               </div>
               <div>
                 <button
                   style={{
-                    marginLeft:"10%",
-                    paddingLeft:"14%",
-                    paddingRight:"14%",
+                    marginLeft: "10%",
+                    paddingLeft: "14%",
+                    paddingRight: "14%",
                     borderRadius: "12px",
                     backgroundColor: "#FEC400",
                     color: "#ffffff",
                     borderStyle: "none",
-                    paddingTop:"1%",
-                    paddingBottom:"1%",
-                    fontSize:"1em",
+                    paddingTop: "1%",
+                    paddingBottom: "1%",
+                    fontSize: "1em",
                   }}
-                > 
+                >
                   Login
                 </button>
-                {error &&
-                <p style={{marginLeft:"10%",color:"red",fontSize:"15px"}}>*{errorMsg}</p>}
+                {error && (
+                  <p
+                    style={{
+                      marginLeft: "10%",
+                      color: "red",
+                      fontSize: "15px",
+                    }}
+                  >
+                    *{errorMsg}
+                  </p>
+                )}
               </div>
             </form>
           </div>
